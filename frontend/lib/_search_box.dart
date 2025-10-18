006e95fc19d9b6846e3148df60b08fab779e37d7
N/A <N/A>
2025-10-18 11:13:54 -0400

    overlay visiblity logic

--- frontend/lib/_search_box.dart
+++ frontend/lib/_search_box.dart
@@ -2,6 +2,67 @@ import 'dart:convert' show jsonEncode;
 import 'package:web_socket_channel/web_socket_channel.dart';
 import 'package:flutter/material.dart';
 
+class TextInput extends StatelessWidget {
+  final _textController = TextEditingController();
+  final WebSocketChannel? _channel;
+  final _overlayController = OverlayPortalController();
+  final _link = LayerLink();
+
+  final FocusNode _textFieldFocusNode = FocusNode();
+  final FocusNode _overlayFocusNode = FocusNode();
+
+  TextInput(this._channel, {super.key});
+
+  void _sendQuery() {
+    final message = {"Type": "query", "Value": _textController.text};
+    _channel?.sink.add(jsonEncode(message));
+    _textController.text = "";
+    _updateVisibilityChange();
+  }
+
+  void _updateVisibilityChange() {
+    final tfHasFocus = _textFieldFocusNode.hasFocus;
+    final oHasFocus = _overlayFocusNode.hasFocus;
+    final hasText = _textController.text.isNotEmpty;
+
+    if ((tfHasFocus || oHasFocus) && hasText) {
+      _overlayController.show();
+    } else {
+      _overlayController.hide();
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    _textFieldFocusNode.addListener(_updateVisibilityChange);
+    _overlayFocusNode.addListener(_updateVisibilityChange);
+    _textController.addListener(_updateVisibilityChange);
+
+    return OverlayPortal(
+      controller: _overlayController,
+      overlayChildBuilder: (context) => UnconstrainedBox(
+        child: CompositedTransformFollower(
+          link: _link,
+          targetAnchor: Alignment.bottomLeft,
+          child: const SizedBox(width: 550, height: 200, child: Placeholder()),
+        ),
+      ),
+      child: CompositedTransformTarget(
+        link: _link,
+        child: TextField(
+          focusNode: _textFieldFocusNode,
+          controller: _textController,
+          decoration: InputDecoration(
+            border: OutlineInputBorder(),
+            hintText: 'Ex: blue_sky cloud 1girl',
+            suffix: IconButton(icon: Icon(Icons.search), onPressed: _sendQuery),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
 class SearchBox extends StatefulWidget {
   final WebSocketChannel? _channel;
   const SearchBox(this._channel, {super.key});
@@ -19,25 +80,13 @@ class _SearchBox extends State<SearchBox> {
     super.dispose();
   }
 
-  void _sendQuery() {
-    final message = {"Type": "query", "Value": controller.text};
-    widget._channel?.sink.add(jsonEncode(message));
-  }
-
   @override
   Widget build(BuildContext context) {
     return Focus(
       onKeyEvent: (node, event) {
         return KeyEventResult.ignored;
       },
-      child: TextField(
-        controller: controller,
-        decoration: InputDecoration(
-          border: OutlineInputBorder(),
-          hintText: 'Ex: blue_sky cloud 1girl',
-          suffix: IconButton(icon: Icon(Icons.search), onPressed: _sendQuery),
-        ),
-      ),
+      child: TextInput(widget._channel),
     );
   }
 }
