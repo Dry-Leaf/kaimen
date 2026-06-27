4a5cc62d65776d19b890a65786f6c09adff9d846
n/a <n/a>
2026-06-27 17:38:57 -0400

    appimage script edit

--- make-appimage.sh
+++ make-appimage.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 set -eux
 
-ARCH="$(uname -m)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+cd "$(dirname "$0")"
 
-# Configure the AppImage
+
 export ICON=kaimen.png
 export DESKTOP=kaimen.desktop
 export OUTPATH=./dist
 export OUTNAME=Kaimen.AppImage
 
-# Download and run quick-sharun
+
+export BIN="bin/kaimen"
+
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
+
+
 wget "$SHARUN" -O ./quick-sharun
 chmod +x ./quick-sharun
 
-# Bundle the application
-./quick-sharun ./dist/AppDir
 
-# Create the AppImage
+rm -rf ./dist/AppDir/.sharun ./dist/AppDir/AppRun
+
+./quick-sharun ./dist/AppDir ./dist/AppDir/search
+
 ./quick-sharun --make-appimage
